9b8e783e08b253e701b026c6bf35c3efb6d3d63a
juancsr <juancsr@protonmail.com>
2021-02-10 16:37:33 -0500

    Versión estable para pdf y correo

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,12 +4,7 @@ const cors = require('cors');
 const app = express(); //instancia de express
 
 //imports
-const personRoutes = require('./routes/person-rotes');
 const FacturaRoutes = require('./routes/factura-rotes');
-const loginRoutes = require('./routes/login-rotes');
-const productoRoutes = require('./routes/producto-route');
-const clienteRoutes = require('./routes/cliente-route');
-const operadorRoutes = require('./routes/operador-route');
 //settings
 app.set('port', process.env.PORT || 3010);
 
@@ -19,12 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use(personRoutes);
 app.use(FacturaRoutes);
-app.use(loginRoutes);
-app.use(productoRoutes);
-app.use(clienteRoutes);
-app.use(operadorRoutes);
 
 //run
 app.listen(app.get('port'), () => {
